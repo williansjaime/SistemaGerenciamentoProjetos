226db8c49dca84b97713b496f732cb9a9ea3a9ea
williansjaime <williansjaime@outlook.com>
2023-11-01 22:31:52 -0300

    montagem das rotas dentro do app modulos e app routing

--- gerenciamentoprojetos/src/app/app.module.ts
+++ gerenciamentoprojetos/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CadastrarprojetosComponent } from './components/template/CadastrarProjetos/cadastrarprojetos/cadastrarprojetos.component';
+
+const routes: Routes = [
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CadastrarprojetosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes, { useHash: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
