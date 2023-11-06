92db03d6b4398f695cd6a1b2ea516928d6661d0e
williansjaime <williansjaime@outlook.com>
2023-11-06 09:26:54 -0300

    Atualizando o pacote de biblioteca

--- FrontEnd/gerenciamentoprojetos/src/app/components/services/LoginService/loginservice.service.ts
+++ FrontEnd/gerenciamentoprojetos/src/app/components/services/LoginService/loginservice.service.ts
@@ -12,6 +12,19 @@ export class LoginserviceService {
 
   constructor() { 
     this.url_API = "/api/v1/login";
+    window.localStorage.setItem('token', '');    
+  }
+
+  
+  getToken(){
+    return String(localStorage.getItem('token'));
+  }
+  getUser(){
+    return String(localStorage.getItem('user'));
+  }
+  get isLoggedIn(): boolean {
+    let authToken =  this.getToken();
+    return authToken != null && authToken != "" ? true : false;
   }
 
   async POST(dataJSON:Login[])
@@ -27,7 +40,17 @@ export class LoginserviceService {
         };
         const data = await fetch(API_URL+this.url_API, requestOptions)
         .then(response => response.json());
-        return data;
+        if(data != null){
+          if (data.status >= 200 && data.status < 300) 
+          {
+            const data_token  = await  data.json();
+            localStorage.setItem('token',data_token[0].token);
+            localStorage.setItem('user',dataJSON[0].usuario);
+            return data_token;
+          }else{
+            return data;
+          }  
+        } 
     } catch (error) {
       return error;
     }  
